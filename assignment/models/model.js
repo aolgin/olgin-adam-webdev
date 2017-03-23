87df28c8cc988ba32a74883d3622a2504f0c1ae3
Adam Olgin <aolgin@ccs.neu.edu>
2017-03-22 21:40:53 -0400

    Just remembered to actually hook it up to MLAB. Should (hopefully) fix the problem

--- assignment/models/model.js
+++ assignment/models/model.js
@@ -6,7 +6,13 @@ module.exports = function() {
     mongoose.Promise = require('q').Promise;
 
     var connectionString = 'mongodb://localhost/web-app-maker';
-    console.log("Connecting to database at " + connectionString);
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
     mongoose.connect(connectionString);
     mongojs('web-app-maker');
 
